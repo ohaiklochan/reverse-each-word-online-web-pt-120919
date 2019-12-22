6e3ae8498e905be66f141187a7547a8f80e2afa0
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 21:32:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
   array.each do |sentence|
-    new_array << array.reverse
+    new_array << sentence.reverse
   end
   array.join(" ")
 end
